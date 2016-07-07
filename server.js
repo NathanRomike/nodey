e46a53d5c23345fb9f995d9d75e02f072a7e2fb0
NathanRomike <nathanromike@gmail.com>
2016-07-06 21:17:16 -0700

    Add simple html message within server response.

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ var nr = require('newrelic');
 var http = require("http"),
     port = process.env.PORT || 1881;
 
+var randomNumber = Math.floor(Math.random() * 999999);
+
+var formattedNumber = randomNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+var htmlBody = '<html><head><meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/></head><body><h1>Hello World!</h1><h2>' + formattedNumber + ' people are thinking  "I love node.js" right now!</h2></body></html>';
+
 var server = http.createServer(function(request, response) {
-  response.writeHeader(200, {"Content-Type": "text/plain"});
-  response.write("Hello HTTP!");
+  response.writeHeader(200, {"Content-Type": "text/html"});
+  response.write(htmlBody);
   response.end();
 });
 
